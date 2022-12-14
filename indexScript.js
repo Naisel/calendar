959623d95e9ca35b8c19f77b1b511c281571eb2e
Naisel <naisel329@gmail.com>
2022-12-14 18:42:19 +0530

    final update

--- indexScript.js
+++ indexScript.js
@@ -3,8 +3,11 @@ const daysTag = document.querySelector(".days");
 const prevNextIcon = document.querySelectorAll(".icons span");
 
 let date = new Date();
-let currYear = date.getFullYear();
-let currMonth = date.getMonth();
+let today = date.getDate();
+let todayMonth = date.getMonth();
+let todayYear = date.getFullYear();
+let currYear = todayYear;
+let currMonth = todayMonth;
 
 const months = [
   "January",
@@ -26,37 +29,46 @@ const renderCalender = () => {
   let firstDate = new Date(currYear, currMonth, 1);
   let lastDateofMonth = lastDate.getDate();
   let firstDayofMonth = firstDate.getDay();
-  let lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
 
+  let lastDayofMonth = lastDate.getDay();
+  let lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
   let liTag = "";
   let i = 0;
-  firstDayofMonth = firstDayofMonth % 6;
   console.log(lastDateofLastMonth);
-  console.log(firstDayofMonth);
-  for (
-    i = lastDateofLastMonth - firstDayofMonth + 1;
-    i <= lastDateofLastMonth;
-    i++
-  ) {
-    liTag += `<li class="inactive">${i}</li>`;
+  console.log(lastDateofMonth);
+  for (i = firstDayofMonth; i > 0; i--) {
+    liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
   }
   for (i = 1; i <= lastDateofMonth; i++) {
-    liTag += `<li>${i}</li>`;
+    let isToday =
+      i === today && currMonth === todayMonth && currYear === todayYear
+        ? "active"
+        : "";
+    liTag += `<li class=${isToday}>${i}</li>`;
+  }
+  for (i = lastDayofMonth; i < 6; i++) {
+    liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`;
   }
   currentDate.innerText = `${months[currMonth]} ${currYear}`;
   daysTag.innerHTML = liTag;
 };
 
 renderCalender();
-let firstDay = new Date(currYear, currMonth, 1);
-let lastDay = new Date(currYear, currMonth + 1, 0);
-console.log(months[currMonth]);
-console.log(lastDay.getDate());
 
 prevNextIcon.forEach((icon) => {
   icon.addEventListener("click", () => {
     currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+    console.log(currYear);
+    if (currMonth < 0 || currMonth > 11) {
+      date = new Date(currYear, currMonth);
+      currYear = date.getFullYear();
+      currMonth = date.getMonth();
+      console.log("here" + date);
+    } else {
+      date = new Date();
+      console.log("there" + date);
+    }
+    console.log("last" + date);
     renderCalender();
-    console.log(icon);
   });
 });
